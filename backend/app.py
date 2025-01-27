bf659ebe9ac00bdf4fc81763f35a42e4cbdede5d
RoseBud31 <dheeja31@gmail.com>
2025-01-27 10:02:38 +0530

    made basic structure of backend, frontend, connected supabase and completed model

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,34 @@
 from flask import Flask, request, jsonify
-from supabase import create_client
-from supabase import Client
+from flask_cors import CORS
+from supabase_client import supabase
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def home():
-    return "Flask Backend is running!"
+@app.route('/register', methods=['POST'])
+# Register a new user
+def register_user():
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+    try:
+        response = supabase.auth.sign_up({"email":email, "password":password})
+        return jsonify({"message":"User registered successfully"})
+    except Exception as e:
+        return jsonify({"message":str(e)})
+
+@app.route('/login', methods=['POST'])
+# Login a user
+def login_user():
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+    try:
+        response = supabase.auth.sign_in_with_password({"email": email, "password": password})
+        return jsonify({"message":"User signed in successfully"})
+    except Exception as e:
+        return jsonify({"message":str(e)})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/superbase.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-from supabase import create_client
-
-load_dotenv()
-SUPERBASE_URL = os.getenv("SUPERBASE_URL")
-SUPERBASE_KEY = os.getenv("SUPERBASE_ANON_KEY")
-
-create_client(SUPERBASE_URL, SUPERBASE_KEY)
